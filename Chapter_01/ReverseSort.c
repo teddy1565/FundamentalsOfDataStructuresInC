4ae0531ab5b8607f2edd82f96c02555091114433
teddy1565 <teddy1565@gmail.com>
2021-01-06 15:07:38 +0800

    ReverseSort

---
+++ Chapter_01/ReverseSort.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))
+void perm(char *list,int i,int n){
+    int j,temp;
+    if(i==n){
+        for(j=0;j<=n;j++){
+            printf("%c",list[j]);
+        }
+        printf(" ");
+    }else{
+        for(j=i;j<=n;j++){
+            SWAP(list[i],list[j],temp);
+            perm(list,i+1,n);
+            SWAP(list[i],list[j],temp);
+        }
+    }
+}
+int main(){
+    return 0;
+}
